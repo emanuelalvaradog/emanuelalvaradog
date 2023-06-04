8cc5783762cc15eea86f4cf15b0e405e7619f8eb
emanuelalvaradog <emanuel.alvarado.g@gmail.com>
2023-06-03 22:20:44 -0600

    fix: change blog metadata

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,41 +5,6 @@ import styles from "@/styles/blog.module.scss";
 import { compareDesc, format, parseISO } from "date-fns";
 import { allPosts, Post } from "contentlayer/generated";
 
-export const generateMetadata = ({ params }) => {
-  return {
-    title: "Blog de Emanuel Alvarado",
-    description:
-      "Mi camino hacia una vida de impacto: pensamientos y aprendizajes",
-    openGraph: {
-      title: "Blog de Emanuel Alvarado",
-      description:
-        "Mi camino hacia una vida de impacto: pensamientos y aprendizajes",
-      type: "website",
-      url: `https://emanuelalvarado.com/blog`,
-      images: [
-        {
-          url: "https://emanuelalvarado.com/emanuelalvarado.jpeg",
-          width: 1920,
-          height: 1080,
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: "Blog de Emanuel Alvarado | @emanuelalvaradog",
-      description:
-        "Mi camino hacia una vida de impacto: pensamientos y aprendizajes",
-      images: [
-        {
-          url: "https://emanuelalvarado.com/emanuelalvarado.jpeg",
-          width: 1920,
-          height: 1080,
-        },
-      ],
-    },
-  };
-};
-
 export default function page() {
   const posts = allPosts.sort((a, b) =>
     compareDesc(parseISO(a.date), parseISO(b.date))
